1017f7acc83cbe7efc4901663e58f9de9dc0f14a
Balinskiy <3879342@gmail.com>
2023-12-18 15:57:05 +0200

    upd-project

---
+++ src/js/swiper.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper('.swiper ', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  speed: 100,
+
+  breakpoints: {
+  375: {
+    slidesPerView: 1,
+  },
+  768: {
+  slidesPerView: 2,
+    spaceBetween: 16
+    },
+  1280: {
+    slidesPerView: 3,
+      spaceBetween: 16
+    }
+},
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './js/mobile_menu.js';
+import './js/rent-pop-up.js';
+import './js/back_to_top.js';
+import './js/swiper.js';
+import './js/reviews-slider';
